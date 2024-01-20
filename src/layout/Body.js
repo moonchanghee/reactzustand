b564d2b3ec40dd17f266584fc2aede2512e0b510
moonchanghee <changhee@urbanbase.com>
2024-01-20 16:50:49 +0900

    refactor: 불필요한 파일을 삭제한다

--- src/layout/Body.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import styled, { css } from 'styled-components';
-
-function Body() {
-    const name = "test";
-    return <div>{name}</div>;
-}
-
-export default Body;
