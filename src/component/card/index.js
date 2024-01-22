b37ab025296c647302f9d63a211f94856d557ef4
moonchanghee <changhee@urbanbase.com>
2024-01-22 15:45:51 +0900

    refactor: 불필요한 코드를 삭제한다

--- src/component/card/index.js
+++ src/component/card/index.js
@@ -1,4 +1,4 @@
-import React , {useState} from "react";
+import React from "react";
 import styled from 'styled-components';
 import { FaStar } from "react-icons/fa";
 import { FaRegStar } from "react-icons/fa6";
