b5f042a0457ab0c3f74f873c1171d214553a25cc
moonchanghee <changhee@urbanbase.com>
2024-01-22 15:46:45 +0900

    feat: 스크랩한 내용이 없을 경우 문구와 버튼을 추가한다

--- src/page/scrap/index.js
+++ src/page/scrap/index.js
@@ -2,6 +2,9 @@ import scrapStore from '../../store/scrapStore';
 import List from '../../component/list'
 import {useInView} from "react-intersection-observer";
 import React from "react";
+import { TiDocumentText } from "react-icons/ti";
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
 
 function Scrap() {
     const { list, getScrapList} = scrapStore();
@@ -16,8 +19,41 @@ function Scrap() {
     });
 
     return (
-        <List list={list} inViewTargetElement={ref} />
+
+        <>
+            {list.length > 0 ?
+                <List list={list} inViewTargetElement={ref}/>
+                :
+                <NoScrapCard>
+                    <TiDocumentText size={50}/>
+                    <h3>저장된 스크랩이 없습니다.</h3>
+                    <GotoScrapButton to={'/'}>스크랩 하러 가기</GotoScrapButton>
+                </NoScrapCard>
+            }
+        </>
     );
 }
 
+
+const NoScrapCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow-y: auto;
+  height: 100%;
+  padding-bottom: 60px;
+  flex-direction: column;
+`;
+
+
+const GotoScrapButton = styled(Link)`
+  padding: 20px 130px;
+  background-color: #2f6cf5;
+  color: white;
+  font-size: 16px;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  text-decoration: none;
+`;
 export default Scrap;
